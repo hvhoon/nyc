735ebd62b55cea8ea1fd0af28177deb9e491c657
Kanav gupta <kanav@relaystrategy.com>
2012-05-31 18:37:52 +0530

    Added A Gradient Layer in List View

--- Soclivity/SectionHeaderView.m
+++ Soclivity/SectionHeaderView.m
@@ -161,21 +161,21 @@
         [disclosureButton addTarget:self action:@selector(toggleOpen:) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:disclosureButton];
         
-        // Set the colors for the gradient layer.
+#endif        // Set the colors for the gradient layer.
         static NSMutableArray *colors = nil;
         if (colors == nil) {
             colors = [[NSMutableArray alloc] initWithCapacity:3];
             UIColor *color = nil;
-            color = [UIColor colorWithRed:0.82 green:0.84 blue:0.87 alpha:1.0];
+            color = [UIColor colorWithRed:1.0 green:1.0 blue:0.99 alpha:1.0];
             [colors addObject:(id)[color CGColor]];
-            color = [UIColor colorWithRed:0.41 green:0.41 blue:0.59 alpha:1.0];
+            color = [UIColor colorWithRed:1.0 green:0.99 blue:1.0 alpha:1.0];
             [colors addObject:(id)[color CGColor]];
-            color = [UIColor colorWithRed:0.41 green:0.41 blue:0.59 alpha:1.0];
+            color = [UIColor colorWithRed:1.0 green:1.0 blue:0.99 alpha:1.0];
             [colors addObject:(id)[color CGColor]];
         }
         [(CAGradientLayer *)self.layer setColors:colors];
         [(CAGradientLayer *)self.layer setLocations:[NSArray arrayWithObjects:[NSNumber numberWithFloat:0.0], [NSNumber numberWithFloat:0.48], [NSNumber numberWithFloat:1.0], nil]];
-#endif        
+        
     }
     
     return self;
