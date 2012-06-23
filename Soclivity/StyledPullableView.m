e4490ac030febd8b702f503224e93b5949aefba4
Harish <harish@soclivity.com>
2012-06-23 03:11:07 -0700

    Tweaked alignment of the dropdown screen

--- Soclivity/StyledPullableView.m
+++ Soclivity/StyledPullableView.m
@@ -102,7 +102,7 @@
         }
         
         // Block of code for the activity types
-        CGRect actTypesLabelRect=CGRectMake(10,100,165,15);
+        CGRect actTypesLabelRect=CGRectMake(10,95,165,15);
         UILabel *actTypesLabel=[[UILabel alloc] initWithFrame:actTypesLabelRect];
         actTypesLabel.textAlignment=UITextAlignmentLeft;
         actTypesLabel.text=[NSString stringWithFormat:@"ACTIVITY TYPES"];
@@ -113,7 +113,7 @@
         [actTypesLabel release];
         
         UIButton *playButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        playButton.frame = CGRectMake(25,130,55,51);
+        playButton.frame = CGRectMake(25,125,55,51);
         playButton.tag=kPlayActivity;
         [playButton setImage:[UIImage imageNamed:@"S04.1_play.png"] forState:UIControlStateNormal];
 
@@ -121,12 +121,12 @@
         [filterPaneView addSubview:playButton];
         
         UIImageView *playTickImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_activityTypeCheck.png"]];
-        playTickImageView.frame=CGRectMake(45, 140, 16, 15);
+        playTickImageView.frame=CGRectMake(45, 135, 16, 15);
         playTickImageView.tag=kPlayTickImage;
         [filterPaneView addSubview:playTickImageView];
         [playTickImageView release];
         
-        CGRect playTypeLabelRect=CGRectMake(25,160,55,15);
+        CGRect playTypeLabelRect=CGRectMake(25,155,55,15);
         UILabel *playTypeLabel=[[UILabel alloc] initWithFrame:playTypeLabelRect];
         playTypeLabel.textAlignment=UITextAlignmentLeft;
         playTypeLabel.text=[NSString stringWithFormat:@"Play"];
@@ -140,7 +140,7 @@
         
         
         UIButton *eatButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        eatButton.frame = CGRectMake(80,130,55,51);
+        eatButton.frame = CGRectMake(80,125,55,51);
         eatButton.tag=kEatActivity;
         [eatButton setImage:[UIImage imageNamed:@"S04.1_eat.png"] forState:UIControlStateNormal];
         
@@ -148,12 +148,12 @@
         [filterPaneView addSubview:eatButton];
         
         UIImageView *eatTickImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_activityTypeCheck.png"]];
-        eatTickImageView.frame=CGRectMake(100, 140, 16, 15);
+        eatTickImageView.frame=CGRectMake(100, 135, 16, 15);
         eatTickImageView.tag=kEatTickImage;
         [filterPaneView addSubview:eatTickImageView];
         [eatTickImageView release];
         
-        CGRect eatTypeLabelRect=CGRectMake(80,160,55,15);
+        CGRect eatTypeLabelRect=CGRectMake(80,155,55,15);
         UILabel *eatTypeLabel=[[UILabel alloc] initWithFrame:eatTypeLabelRect];
         eatTypeLabel.textAlignment=UITextAlignmentLeft;
         eatTypeLabel.text=[NSString stringWithFormat:@"Eat"];
@@ -166,7 +166,7 @@
         [eatTypeLabel release];
         
         UIButton *seeButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        seeButton.frame = CGRectMake(135,130,55,51);
+        seeButton.frame = CGRectMake(135,125,55,51);
         seeButton.tag=kSeeActivity;
         [seeButton setImage:[UIImage imageNamed:@"S04.1_see.png"] forState:UIControlStateNormal];
         
@@ -174,12 +174,12 @@
         [filterPaneView addSubview:seeButton];
         
         UIImageView *seeTickImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_activityTypeCheck.png"]];
-        seeTickImageView.frame=CGRectMake(155, 140, 16, 15);
+        seeTickImageView.frame=CGRectMake(155, 135, 16, 15);
         seeTickImageView.tag=kSeeTickImage;
         [filterPaneView addSubview:seeTickImageView];
         [seeTickImageView release];
         
-        CGRect seeTypeLabelRect=CGRectMake(135,160,55,15);
+        CGRect seeTypeLabelRect=CGRectMake(135,155,55,15);
         UILabel *seeTypeLabel=[[UILabel alloc] initWithFrame:seeTypeLabelRect];
         seeTypeLabel.textAlignment=UITextAlignmentLeft;
         seeTypeLabel.text=[NSString stringWithFormat:@"See"];
@@ -193,7 +193,7 @@
         
         
         UIButton *createButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        createButton.frame = CGRectMake(190,130,55,51);
+        createButton.frame = CGRectMake(190,125,55,51);
         createButton.tag=kCreateActivity;
         [createButton setImage:[UIImage imageNamed:@"S04.1_create.png"] forState:UIControlStateNormal];
         
@@ -201,12 +201,12 @@
         [filterPaneView addSubview:createButton];
         
         UIImageView *createTickImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_activityTypeCheck.png"]];
-        createTickImageView.frame=CGRectMake(210, 140, 16, 15);
+        createTickImageView.frame=CGRectMake(210, 135, 16, 15);
         createTickImageView.tag=kCreateTickImage;
         [filterPaneView addSubview:createTickImageView];
         [createTickImageView release];
         
-        CGRect createTypeLabelRect=CGRectMake(190,160,55,15);
+        CGRect createTypeLabelRect=CGRectMake(190,155,55,15);
         UILabel *createTypeLabel=[[UILabel alloc] initWithFrame:createTypeLabelRect];
         createTypeLabel.textAlignment=UITextAlignmentLeft;
         createTypeLabel.text=[NSString stringWithFormat:@"Create"];
@@ -220,7 +220,7 @@
         
         
         UIButton *learnButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        learnButton.frame = CGRectMake(245,130,55,51);
+        learnButton.frame = CGRectMake(245,125,55,51);
         learnButton.tag=kLearnActivity;
         [learnButton setImage:[UIImage imageNamed:@"S04.1_learn.png"] forState:UIControlStateNormal];
         
@@ -228,12 +228,12 @@
         [filterPaneView addSubview:learnButton];
         
         UIImageView *learnTickImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_activityTypeCheck.png"]];
-        learnTickImageView.frame=CGRectMake(265, 140, 16, 15);
+        learnTickImageView.frame=CGRectMake(265, 135, 16, 15);
         learnTickImageView.tag=kLearnTickImage;
         [filterPaneView addSubview:learnTickImageView];
         [learnTickImageView release];
         
-        CGRect learnTypeLabelRect=CGRectMake(245,160,55,15);
+        CGRect learnTypeLabelRect=CGRectMake(245,155,55,15);
         UILabel *learnTypeLabel=[[UILabel alloc] initWithFrame:learnTypeLabelRect];
         learnTypeLabel.textAlignment=UITextAlignmentLeft;
         learnTypeLabel.text=[NSString stringWithFormat:@"Learn"];
@@ -247,29 +247,29 @@
         
         // Dates selection section
         UIImageView *calendarIconImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_calendarIcon.png"]];
-        calendarIconImageView.frame=CGRectMake(10, 207, 26, 25);
+        calendarIconImageView.frame=CGRectMake(10, 197, 26, 25);
         [filterPaneView addSubview:calendarIconImageView];
         [calendarIconImageView release];
         
         UIImageView *horizontalDividerImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_horizontalDivider.png"]];
-        horizontalDividerImageView.frame=CGRectMake(41, 217, 269, 1);
+        horizontalDividerImageView.frame=CGRectMake(41, 207, 269, 1);
         [filterPaneView addSubview:horizontalDividerImageView];
         [horizontalDividerImageView release];
         
         
         UIButton *next2DaysButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        next2DaysButton.frame = CGRectMake(12,238,100,40);
+        next2DaysButton.frame = CGRectMake(12,228,100,40);
         next2DaysButton.tag=kNextTwoDays;
         [next2DaysButton addTarget:self action:@selector(activityButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [filterPaneView addSubview:next2DaysButton];
         
         UIImageView *next2DaysTickImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_datesCheck.png"]];
-        next2DaysTickImageView.frame=CGRectMake(54, 238, 16, 15);
+        next2DaysTickImageView.frame=CGRectMake(54, 228, 16, 15);
         next2DaysTickImageView.tag=kNextTwoDaysTickImage;
         [filterPaneView addSubview:next2DaysTickImageView];
         [next2DaysTickImageView release];
         
-        CGRect next2DaysLabelRect=CGRectMake(12,263,100,15);
+        CGRect next2DaysLabelRect=CGRectMake(12,253,100,15);
         UILabel *next2DaysLabel=[[UILabel alloc] initWithFrame:next2DaysLabelRect];
         next2DaysLabel.textAlignment=UITextAlignmentCenter;
         next2DaysLabel.text=[NSString stringWithFormat:@"Next 2 Days"];
@@ -282,25 +282,25 @@
         
         
         UIImageView *datesVerticalDividerImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_datesVerticalDivider.png"]];
-        datesVerticalDividerImageView.frame=CGRectMake(112, 243, 1, 29);
+        datesVerticalDividerImageView.frame=CGRectMake(112, 233, 1, 29);
         [filterPaneView addSubview:datesVerticalDividerImageView];
         [datesVerticalDividerImageView release];
         
         
         UIButton *next7DaysButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        next7DaysButton.frame = CGRectMake(113,238,100,40);
+        next7DaysButton.frame = CGRectMake(113,228,100,40);
         next7DaysButton.tag=kNextSevenDays;
         [next7DaysButton addTarget:self action:@selector(activityButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [filterPaneView addSubview:next7DaysButton];
         
         UIImageView *next7DaysTickImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_datesCheck.png"]];
-        next7DaysTickImageView.frame=CGRectMake(155, 238, 16, 15);
+        next7DaysTickImageView.frame=CGRectMake(155, 228, 16, 15);
         next7DaysTickImageView.tag=kNextSevenDaysTickImage;
         next7DaysTickImageView.alpha=0.3f;
         [filterPaneView addSubview:next7DaysTickImageView];
         [next7DaysTickImageView release];
         
-        CGRect next7DaysLabelRect=CGRectMake(113,263,100,15);
+        CGRect next7DaysLabelRect=CGRectMake(113,253,100,15);
         UILabel *next7DaysLabel=[[UILabel alloc] initWithFrame:next7DaysLabelRect];
         next7DaysLabel.textAlignment=UITextAlignmentCenter;
         next7DaysLabel.text=[NSString stringWithFormat:@"Next 7 Days"];
@@ -314,24 +314,24 @@
         
         
         UIImageView *datesVerticalDividerImageView2=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_datesVerticalDivider.png"]];
-        datesVerticalDividerImageView2.frame=CGRectMake(213, 243, 1, 29);
+        datesVerticalDividerImageView2.frame=CGRectMake(213, 233, 1, 29);
         [filterPaneView addSubview:datesVerticalDividerImageView2];
         [datesVerticalDividerImageView2 release];
         
         UIButton *pickADayButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        pickADayButton.frame = CGRectMake(214,238,100,45);
+        pickADayButton.frame = CGRectMake(214,228,100,45);
         pickADayButton.tag=kPickADay;
         [pickADayButton addTarget:self action:@selector(activityButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [filterPaneView addSubview:pickADayButton];
         
         UIImageView *pickADayTickImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_pickADayIcon.png"]];
-        pickADayTickImageView.frame=CGRectMake(257, 238, 14, 15);
+        pickADayTickImageView.frame=CGRectMake(257, 228, 14, 15);
         pickADayTickImageView.tag=kPickADayTickImage;
         pickADayTickImageView.alpha=0.3f;
         [filterPaneView addSubview:pickADayTickImageView];
         [pickADayTickImageView release];
         
-        CGRect pickADayLabelRect=CGRectMake(230,263,100,15);
+        CGRect pickADayLabelRect=CGRectMake(230,253,100,15);
         UILabel *pickADayLabel=[[UILabel alloc] initWithFrame:pickADayLabelRect];
         pickADayLabel.textAlignment=UITextAlignmentLeft;
         pickADayLabel.text=[NSString stringWithFormat:@"Pick A Day"];
@@ -345,28 +345,28 @@
         
         // Time selection section
         UIImageView *clockIconImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_clock.png"]];
-        clockIconImageView.frame=CGRectMake(10, 302, 24, 23);
+        clockIconImageView.frame=CGRectMake(10, 292, 24, 23);
         [filterPaneView addSubview:clockIconImageView];
         [clockIconImageView release];
         
         UIImageView *horizontalDividerImageView2=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_horizontalDivider.png"]];
-        horizontalDividerImageView2.frame=CGRectMake(44, 309, 269, 1);
+        horizontalDividerImageView2.frame=CGRectMake(44, 299, 269, 1);
         [filterPaneView addSubview:horizontalDividerImageView2];
         [horizontalDividerImageView2 release];
         
         UIButton *morningButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        morningButton.frame =CGRectMake(12,330,100,55);
+        morningButton.frame =CGRectMake(12,320,100,55);
         morningButton.tag=kMorningButton;
         [morningButton addTarget:self action:@selector(activityButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [filterPaneView addSubview:morningButton];
         
         UIImageView *sunriseImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_sunriseSelected.png"]];
-        sunriseImageView.frame=CGRectMake(55, 330, 15,9);
+        sunriseImageView.frame=CGRectMake(55, 320, 15,9);
         sunriseImageView.tag=kSunriseImage;
         [filterPaneView addSubview:sunriseImageView];
         [sunriseImageView release];
         
-        CGRect morningLabelRect=CGRectMake(12,345,100,15);
+        CGRect morningLabelRect=CGRectMake(12,335,100,15);
         UILabel *morningLabel=[[UILabel alloc] initWithFrame:morningLabelRect];
         morningLabel.textAlignment=UITextAlignmentCenter;
         morningLabel.text=[NSString stringWithFormat:@"Morning"];
@@ -377,7 +377,7 @@
         [filterPaneView addSubview:morningLabel];
         [morningLabel release];
         
-        CGRect Am12_Am11_LabelRect=CGRectMake(12,365,100,15);
+        CGRect Am12_Am11_LabelRect=CGRectMake(12,355,100,15);
         UILabel *Am12_Am11Label=[[UILabel alloc] initWithFrame:Am12_Am11_LabelRect];
         Am12_Am11Label.textAlignment=UITextAlignmentCenter;
         Am12_Am11Label.text=[NSString stringWithFormat:@"12am - 11am"];
@@ -390,23 +390,23 @@
         
         
         UIImageView *timeVerticalDividerImageView1=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_timeVerticalDivider.png"]];
-        timeVerticalDividerImageView1.frame=CGRectMake(112, 333, 1, 41);
+        timeVerticalDividerImageView1.frame=CGRectMake(112, 323, 1, 41);
         [filterPaneView addSubview:timeVerticalDividerImageView1];
         [timeVerticalDividerImageView1 release];
 
         UIButton *afternoonButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        afternoonButton.frame =CGRectMake(113,330,100,55);
+        afternoonButton.frame =CGRectMake(113,320,100,55);
         afternoonButton.tag=kAfternoonButton;
         [afternoonButton addTarget:self action:@selector(activityButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [filterPaneView addSubview:afternoonButton];
         
         UIImageView *sunSelectedImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_sunSelected.png"]];
-        sunSelectedImageView.frame=CGRectMake(155, 324, 16, 15);
+        sunSelectedImageView.frame=CGRectMake(155, 314, 16, 15);
         sunSelectedImageView.tag=kSunSelectedImage;
         [filterPaneView addSubview:sunSelectedImageView];
         [sunSelectedImageView release];
         
-        CGRect afternoonLabelRect=CGRectMake(113,345,100,15);
+        CGRect afternoonLabelRect=CGRectMake(113,335,100,15);
         UILabel *afternoonLabel=[[UILabel alloc] initWithFrame:afternoonLabelRect];
         afternoonLabel.textAlignment=UITextAlignmentCenter;
         afternoonLabel.text=[NSString stringWithFormat:@"Afternoon"];
@@ -417,7 +417,7 @@
         [filterPaneView addSubview:afternoonLabel];
         [afternoonLabel release];
         
-        CGRect Pm12_Pm6_LabelRect=CGRectMake(113,365,100,15);
+        CGRect Pm12_Pm6_LabelRect=CGRectMake(113,355,100,15);
         UILabel *Pm12_Pm6_Label=[[UILabel alloc] initWithFrame:Pm12_Pm6_LabelRect];
         Pm12_Pm6_Label.textAlignment=UITextAlignmentCenter;
         Pm12_Pm6_Label.text=[NSString stringWithFormat:@"12pm - 6pm"];
@@ -429,23 +429,23 @@
         [Pm12_Pm6_Label release];
         
         UIImageView *timeVerticalDividerImageView2=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_timeVerticalDivider.png"]];
-        timeVerticalDividerImageView2.frame=CGRectMake(213, 333, 1, 41);
+        timeVerticalDividerImageView2.frame=CGRectMake(213, 323, 1, 41);
         [filterPaneView addSubview:timeVerticalDividerImageView2];
         [timeVerticalDividerImageView2 release];
         
         UIButton *eveningButton = [[UIButton buttonWithType:UIButtonTypeCustom] retain];
-        eveningButton.frame =CGRectMake(214,330,100,55);
+        eveningButton.frame =CGRectMake(214,320,100,55);
         eveningButton.tag=kEveningButton;
         [eveningButton addTarget:self action:@selector(activityButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
         [filterPaneView addSubview:eveningButton];
         
         UIImageView *moonSelectedImageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"S04.1_moonSelected.png"]];
-        moonSelectedImageView.frame=CGRectMake(255, 324, 13, 15);
+        moonSelectedImageView.frame=CGRectMake(255, 314, 13, 15);
         moonSelectedImageView.tag=kMoonSelectedImage;
         [filterPaneView addSubview:moonSelectedImageView];
         [moonSelectedImageView release];
         
-        CGRect eveningLabelRect=CGRectMake(214,345,100,15);
+        CGRect eveningLabelRect=CGRectMake(214,335,100,15);
         UILabel *eveningLabel=[[UILabel alloc] initWithFrame:eveningLabelRect];
         eveningLabel.textAlignment=UITextAlignmentCenter;
         eveningLabel.text=[NSString stringWithFormat:@"Evening"];
@@ -456,7 +456,7 @@
         [filterPaneView addSubview:eveningLabel];
         [eveningLabel release];
         
-        CGRect Pm7_Pm11_LabelRect=CGRectMake(214,365,100,15);
+        CGRect Pm7_Pm11_LabelRect=CGRectMake(214,355,100,15);
         UILabel *Pm7_Pm11_Label=[[UILabel alloc] initWithFrame:Pm7_Pm11_LabelRect];
         Pm7_Pm11_Label.textAlignment=UITextAlignmentCenter;
         Pm7_Pm11_Label.text=[NSString stringWithFormat:@"7pm - 11pm"];
