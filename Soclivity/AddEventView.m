4fbf3fb8b867e2bcb373801b293c0aea66c11082
Kanav Gupta <apple@apples-MacBook-Pro-3.local>
2013-05-02 00:27:25 +0530

    Add event Pressed State Added

--- Soclivity/AddEventView.m
+++ Soclivity/AddEventView.m
@@ -1606,11 +1606,11 @@ else {
                 }
                 
                 // Showing rating information - Crashing the program
-                //ratingLabel.text=loc.annotation.ratingValue;
-                //[delegate enableDisableTickOnTheTopRight:YES];
+                ratingLabel.text=loc.annotation.ratingValue;
+                [delegate enableDisableTickOnTheTopRight:YES];
                 
-                ratingLabel.text=[NSString stringWithFormat:@"Rating: N/A"];
-                [self getFourSquareRating:loc.annotation];
+                //ratingLabel.text=[NSString stringWithFormat:@"Rating: N/A"];
+                //[self getFourSquareRating:loc.annotation];
             }
                 break;
                 
@@ -2542,14 +2542,13 @@ else {
             placemark.queryName=[pins objectForKey:@"name"];
             placemark.foursquareId=[pins objectForKey:@"id"];
             
-            /*
+            
             // Pulling rating information
             if([pins objectForKey:@"rating"]!=nil && [[pins objectForKey:@"rating"] class]!=[NSNull null]) {
                 placemark.ratingValue=[NSString stringWithFormat:@"Rating: %@",[[pins objectForKey:@"rating"]stringValue]];
             }
             else
                 placemark.ratingValue=[NSString stringWithFormat:@"Rating: N/A"];
-            */
             
              
             if([[pins objectForKey:@"contact"]objectForKey:@"phone"]!=nil && [[[pins objectForKey:@"contact"]objectForKey:@"phone"] class]!=[NSNull null]){
