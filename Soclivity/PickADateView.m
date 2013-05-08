6c8578b8dcb3c85b79d80f49f60653a72fab35ee
Harish <harish@soclivity.com>
2013-05-08 02:45:19 -0400

    Updated the create and edit activity screens for New Look

--- Soclivity/PickADateView.m
+++ Soclivity/PickADateView.m
@@ -66,7 +66,7 @@
     pickADateLabel.shadowOffset = CGSizeMake(0,-1);
 
     
-    calendarDate=[[CalendarDateView alloc]initWithFrame:CGRectMake(0, 44, 320,304)editActivityDate:activityDate];
+    calendarDate=[[CalendarDateView alloc]initWithFrame:CGRectMake(0, 40, 320,304)editActivityDate:activityDate];
     calendarDate.KALDelegate=self;
     calendarDate.pickADateForActivity=TRUE;
     calendarDate.activityTime=activityTime;
